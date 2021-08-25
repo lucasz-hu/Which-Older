541ba28da21b9d3907da36fc49cf99247a82bdbe
unknown <laz51@pitt.edu>
2021-08-24 22:28:53 -0400

    bug fixes, also added print_right and print_wrong functions for more functionality

--- main.py
+++ main.py
@@ -13,116 +13,129 @@ months = ["January", "February", "March", "April", "May", "June", "July", "Augus
 def main():
     print("Welcome to the historical over-under guessing game!")
     print("The objective is to guess which item is older?")
-    statesGuess()
+    states_guess()
 
-def statesGuess():
-    print("Which state is older?", end=": ")
+def states_guess():
+    score = 0;
     selections = random.sample(list(states), 2)
     while states[selections[1]] == states[selections[0]]:       #check that the dates arent the same
         selections = random.sample(list(states), 2)
     print(selections[0], end=" vs. ")
     print(selections[1])
-    userInput = input("")
-    userInput = userInput.title()
+    userInput = input("Which state is older?: ")
     if userInput == "1":
         userInput = selections[0]
     elif userInput == "2":
         userInput = selections[1] 
     while userInput not in states.keys():                               #check that input is valid
-        print("Input not recognized, please input again:")
-        userInput = input("")
-        userInput = userInput.title()
-    correctAnswer = grade_date(selections[0], selections[1]);
+        userInput = input("Input not recognized, please input again: ")
+    correctAnswer = grade_date(selections[0], selections[1], states);
+    wrongAnswer = wrong_date(correctAnswer, selections[0], selections[1])
     if userInput.lower() == correctAnswer.lower():
-        print("Correct")
+        print_correct(correctAnswer, wrongAnswer, states)
+        score+=1;
+        print("Score: " + str(score))
+    else:
+        print_wrong(correctAnswer, wrongAnswer, states)
+        score = 0
+        print("Score: " + str(score))
+    states_guess_override(selections[1], score)
+    
+# function used for state game after initial state has been run
+# previousstate (first arg) is the second date from the previous state game
+# score (second arg) is score of user
+# selection (inside function) is the newly randomly chosen state
+def states_guess_override(previousState, score):
+    selectionArray = random.sample(list(states), 1)
+    selection = selectionArray[0]
+    while selection == previousState:
+        selectionArray = random.sample(list(states), 1)
+        selection = selectionArray[0]
+    print(previousState, end = " vs. ")
+    print(selection)
+    userInput = input("Which state is older?: ")
+    while userInput not in states.keys():
+        userInput = input("Input not recognized, please input again: ")
+    correctAnswer = grade_date(previousState, selection, states)
+    wrongAnswer = wrong_date(correctAnswer, previousState, selection)
+    if userInput.lower() == correctAnswer.lower():
+        print_correct(correctAnswer, wrongAnswer, states)
+        score+=1;
+        print("Score: " + str(score))
+    else:
+        print_wrong(correctAnswer, wrongAnswer, states)
+        score=0;
+        print("Score: " + str(score))
+    states_guess_override(selection, score)
 
-    # if userInput.lower() == selections[0].lower(): #if user is first input
-    #     if states[userInput][6:10] == states[selections[1]][6:10]: #if userInputYear < otherItemYear (checks if same year)
-    #         if states[userInput][3:5] == states[selections[1]][3:5]: #if userInputMonth == otherInputMonth (checks if same month)
-    #             if states[userInput][0:2] < states[selections[1]][0:2]:
-    #                 print("Correct!")
-    #                 printString = selections[0] + "(" + states[selections[0]] + ") is older than " + selections[1] + "(" + states[selections[1]] + ")"
-    #                 print(printString)
-    #                 statesGuess()
-    #             else:
-    #                 print("Incorrect!")
-    #                 printString = selections[1] + "(" + states[selections[1]] + ") is older than " + selections[0] + "(" + states[selections[0]] + ")"
-    #                 print(printString)
-    #                 statesGuess()
-    #         elif states[userInput][3:5] < states[selections[1]][3:5]: #if userInputMonth < otherItemMonth
-    #             print("Correct!")
-    #             printString = selections[0] + "(" + states[selections[0]] + ") is older than " + selections[1] + "(" + states[selections[1]] + ")"
-    #             print(printString)
-    #             statesGuess()
-    #         else:
-    #             print("Incorrect!")
-    #             printString = selections[1] + "(" + states[selections[1]] + ") is older than " + selections[0] + "(" + states[selections[0]] + ")"
-    #             print(printString)
-    #             statesGuess()
-    #     elif states[userInput][6:10] < states[selections[1]][6:10]: #if userInputYear < otherItemYear
-    #         print("Correct!")
-    #         printString = selections[0] + "(" + states[selections[0]] + ") is older than " + selections[1] + "(" + states[selections[1]] + ")"
-    #         print(printString)
-    #         statesGuess()
-    #     else:
-    #         print("Incorrect!")
-    #         printString = selections[1] + "(" + states[selections[1]] + ") is older than " + selections[0] + "(" + states[selections[0]] + ")"
-    #         print(printString)
-    #         statesGuess()
 
-    # elif userInput.lower() == selections[1].lower(): #if user is second input
-    #     if states[userInput][6:10] == states[selections[0]][6:10]: #if userInputYear < otherItemYear (checks if same year)
-    #         if states[userInput][3:5] == states[selections[0]][3:5]: #if userInputMonth == otherInputMonth (checks if same month)
-    #             if states[userInput][0:2] < states[selections[0]][0:2]:
-    #                 print("Correct!")
-    #                 printString = selections[1] + "(" + states[selections[1]] + ") is older than " + selections[0] + "(" + states[selections[0]] + ")"
-    #                 print(printString)
-    #                 statesGuess()
-    #             else:
-    #                 print("Incorrect!")
-    #                 printString = selections[0] + "(" + states[selections[0]] + ") is older than " + selections[1] + "(" + states[selections[1]] + ")"
-    #                 print(printString)
-    #                 statesGuess()
-    #         elif states[userInput][3:5] < states[selections[0]][3:5]: #if userInputMonth < otherItemMonth
-    #             print("Correct!")
-    #             printString = selections[1] + "(" + states[selections[1]] + ") is older than " + selections[0] + "(" + states[selections[0]] + ")"
-    #             print(printString)
-    #             statesGuess()
-    #         else:
-    #             print("Incorrect!")
-    #             printString = selections[0] + "(" + states[selections[0]] + ") is older than " + selections[1] + "(" + states[selections[1]] + ")"
-    #             print(printString)
-    #             statesGuess()
-    #     elif states[userInput][6:10] < states[selections[0]][6:10]: #if userInputYear < otherItemYear
-    #         print("Correct!")
-    #         printString = selections[1] + "(" + states[selections[1]] + ") is older than " + selections[0] + "(" + states[selections[0]] + ")"
-    #         print(printString)
-    #         statesGuess()
-    #     else:
-    #         print("Incorrect!")
-    #         printString = selections[0] + "(" + states[selections[0]] + ") is older than " + selections[1] + "(" + states[selections[1]] + ")"
-    #         print(printString)
-    #         statesGuess()
 
-def grade_date(firstDate, secondDate):
-    if firstDate[6:10] == secondDate[6:10]:
-        if firstDate[3:5] == secondDate[3:5]:
-            if firstDate[0:2] == secondDate[0:2]:
+# Function to grade which is older, first and second parameters are the first and second dates, 
+# third parameter is which dictionary is being used
+def grade_date(firstDate, secondDate, set):
+    firstYear = set[firstDate][6:10] 
+    firstMonth = set[firstDate][3:5]
+    firstDay = set[firstDate][0:2]
+
+    secondYear = set[secondDate][6:10]
+    secondMonth = set[secondDate][3:5]
+    secondDay = set[secondDate][0:2]
+
+    if firstYear == secondYear:
+        if firstMonth == secondMonth:
+            if firstDay == secondDay:
                 return None;
-            elif firstDate[0:2] > secondDate[0:2]:
+            elif firstDay < secondDay:
                 return firstDate;
             else:
                 return secondDate;
-        elif firstDate[3:5] > secondDate[3:5]:
+        elif firstMonth < secondMonth:
             return firstDate;
         else:
             return secondDate;
-    elif firstDate[6:10] > secondDate[6:10]:
+    elif firstYear < secondYear:
         return firstDate;
     else:
         return secondDate;
 
-                
-                
+# This function is used to output the wrong date to the user once inputted selection
+# first arg is the right answer, second arg is first date, third arg is second date
+def wrong_date(rightDate, firstDate, secondDate):
+    if rightDate == firstDate:
+        return secondDate
+    elif rightDate == secondDate:
+        return firstDate
+    else:
+        return None
+
+# evoked when user inputs correct answer
+# first arg is right answer (this date is older)
+# second arg is wrong answer (this date is younger)
+# set is dict currently being used
+def print_correct(rightAnswer, wrongAnswer, set):
+    rightYear = set[rightAnswer][6:10]
+    rightMonth = months[int(set[rightAnswer][3:5])-1]
+    rightDay = set[rightAnswer][0:2].lstrip("0")
+
+    wrongYear = set[wrongAnswer][6:10]
+    wrongMonth = months[int(set[wrongAnswer][3:5])-1]
+    wrongDay = set[wrongAnswer][0:2].lstrip("0")
+
+    print("Correct! " + rightAnswer + " (" + rightMonth + ", " + rightDay + " " + rightYear + ") is older than " + wrongAnswer + " (" + wrongMonth  + ", " + wrongDay + " " + wrongYear + ")")
+
+#evoked when user inputs wrong answer
+#first arg is wrong answer (This date is younger)
+#second arg is right answer (this date is older)
+#set is dict currently used
+def print_wrong(wrongAnswer, rightAnswer, set):
+    rightYear = set[rightAnswer][6:10]
+    rightMonth = months[int(set[rightAnswer][3:5])-1]
+    rightDay = set[rightAnswer][0:2].lstrip("0")
+
+    wrongYear = set[wrongAnswer][6:10]
+    wrongMonth = months[int(set[wrongAnswer][3:5])-1]
+    wrongDay = set[wrongAnswer][0:2].lstrip("0")
+
+    print("Incorrect! " + wrongAnswer + " (" + wrongMonth + ", " + wrongDay + " " + wrongYear + ") is older than " + rightAnswer + " (" + rightMonth + ", " + rightDay + " " + rightYear + ")")
 
 main()
